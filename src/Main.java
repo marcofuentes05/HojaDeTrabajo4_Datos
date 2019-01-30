0e6a9c048119ddf40b345040cafb6e570ee6ebb6
Marco Fuentes <marcofuentes1607@gmail.com>
2019-01-30 16:48:45 -0600

    Se puede leer el archivo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,20 @@
-public class Main {
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
+public class Main {
+    public static void main(String args[]) {
+        List<String> mapa =  new ArrayList<String>();
+        try{
+            Stream<String> lines = Files.lines(Paths.get("datos.txt"), StandardCharsets.UTF_8);
+            lines.forEach(s  -> mapa.add(s));
+        }catch(IOException e){
+        System.out.println("Error!");
+        }
+
+    }
 }
