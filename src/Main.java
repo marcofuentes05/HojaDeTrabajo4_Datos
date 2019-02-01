76ee8eb7eb66e6ff8a4c12739010a95c716633f1
Marco Fuentes <marcofuentes1607@gmail.com>
2019-01-31 18:56:31 -0600

    Avances

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,24 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String args[]) {
         List<String> mapa =  new ArrayList<String>();
+        CalculatorImp calculadora = new CalculatorImp();
+        Fila <String> operadores = new Fila<>();
+        Fila <Integer> operandos = new Fila<>();
+
         try{
             Stream<String> lines = Files.lines(Paths.get("datos.txt"), StandardCharsets.UTF_8);
             lines.forEach(s  -> mapa.add(s));
         }catch(IOException e){
-        System.out.println("Error!");
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
+
+        int size = mapa.size();
+
+        for (int i = 0;i <size;i++){
+            if (mapa.get(1).equals(" ")){
+
+            }
         }
 
     }
